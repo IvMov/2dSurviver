4eb7300452d46d96b122406e8d7b17ca8ee473b5
IvanMovchanets <imovchanets@reiz.tech>
2024-01-08 19:14:45 +0200

    add autospawn of enemies

---
+++ scenes/manager/enemy_manager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+const SPAWN_RADIUS = 400
+
+@export var basic_enemy_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.timeout.connect(on_timeout)
+	
+
+func on_timeout():
+	var player = get_tree().get_first_node_in_group("player") as Node2D	
+	if !player:
+		return
+		
+	var enemy = basic_enemy_scene.instantiate() as Node2D	
+	var random_direction = get_random_direction();
+	var enemy_spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+	
+	enemy.global_position = enemy_spawn_position
+	get_parent().add_child(enemy)
+	
+	
+func get_random_direction():
+	return Vector2.RIGHT.rotated(randf_range(0, TAU))
