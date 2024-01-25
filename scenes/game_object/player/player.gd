096aaf1e70c7cc16a60acbc1588501f66648ea8e
IvanMovchanets <imovchanets@reiz.tech>
2024-01-25 19:08:35 +0200

    add health reduction to player

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -2,11 +2,13 @@ extends CharacterBody2D
 
 const MAX_SPEED = 150
 const ACCELERATION_SMOOTHING = 10
+@onready var damage_interval_timer = $DamageIntervalTimer
+var number_colliding_bodies: int = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$Area2D.area_entered.connect(handle_pick_up_items)
-
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timeout_timeout)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,5 +25,23 @@ func get_movement_vector():
 	
 	return Vector2(x_movement, y_movement)
 
-func handle_pick_up_items(area):
-	pass
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	$HealthComponent.damage(1)
+	damage_interval_timer.start()
+	print($HealthComponent.current_health)
+	
+	
+func on_body_entered(body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func on_body_exited(body: Node2D):
+	number_colliding_bodies -= 1
+
+func on_damage_interval_timeout_timeout():
+	check_deal_damage()
+	
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -4,7 +4,6 @@ const SPAWN_RADIUS = 400
 
 @export var basic_enemy_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.timeout.connect(on_timeout)
 	
@@ -14,8 +13,8 @@ func on_timeout():
 	if !player:
 		return
 		
-	var enemy = basic_enemy_scene.instantiate() as Node2D	
-	var random_direction = get_random_direction();
+	var enemy = basic_enemy_scene.instantiate() as Node2D
+	var random_direction = get_random_direction()
 	var enemy_spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
 	
 	enemy.global_position = enemy_spawn_position
