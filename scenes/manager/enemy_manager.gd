1da808d80adcdaf702260bd67fdef39aae0f0ed9
IvanMovchanets <imovchanets@reiz.tech>
2024-01-29 18:01:22 +0200

    add wall elements, enemies can't spawn over the wall

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -14,16 +14,31 @@ func _ready():
 	spawn_timer.start()
 	horde_timer.timeout.connect(on_horde_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
-	
+
 	
 func instaintiate_enemy(player: Player):
 	var enemy = basic_enemy_scene.instantiate() as Node2D
-	var random_direction = get_random_direction()
-	var enemy_spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
-	enemy.global_position = enemy_spawn_position
+	enemy.global_position = get_spawn_position(player.global_position, SPAWN_RADIUS)
 	get_tree().get_first_node_in_group("entities_layer").add_child(enemy)
 	EnemyCounter.add_enemy()
 
+
+func get_spawn_position(player_possition: Vector2, spawn_radius: int):
+	var enemy_spawn_position = player_possition + (get_random_direction() * spawn_radius)
+	var query_parameters = PhysicsRayQueryParameters2D.create(player_possition, enemy_spawn_position, 1) #1 - bit value of first terrain (the terrain wich we interested in to check)
+	var direct_space_state = get_tree().root.world_2d.direct_space_state
+	var result = direct_space_state.intersect_ray(query_parameters)
+	var attempts = 0
+	while !result.is_empty():
+		attempts+=1
+		if attempts >= 4:
+			attempts = 0
+			return get_spawn_position(player_possition, max(spawn_radius / 2, 2))
+		enemy_spawn_position = enemy_spawn_position.rotated(PI)
+		query_parameters.set_to(enemy_spawn_position)
+		result = direct_space_state.intersect_ray(query_parameters)
+	return enemy_spawn_position.limit_length(spawn_radius - 1)
+	
 	
 func get_random_direction():
 	return Vector2.RIGHT.rotated(randf_range(0, TAU))
@@ -31,7 +46,6 @@ func get_random_direction():
 	
 func on_spawn_timer_timeout():
 	spawn_timer.start()
-	
 	var player = get_tree().get_first_node_in_group("player") as Player	
 	if !player:
 		return
