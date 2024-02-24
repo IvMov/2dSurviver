459484d7b6bccc698ae70cf9f4d4bfe2a1710263
IvanMovchanets <iv.movchanets@gmail.com>
2024-02-24 10:27:09 +0200

    animate coins and exp vials

--- 2dSurviverSourceCode/scenes/game_object/curency/coin.gd
+++ 2dSurviverSourceCode/scenes/game_object/curency/coin.gd
@@ -2,12 +2,36 @@ extends Node2D
 
 var VALUE: int
 
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
+@onready var animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
 	
+func tween_collect(percent: float, start_position: Vector2):
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if !player:
+		return
+	
+	global_position = start_position.lerp(player.global_position, percent)
+	
+
+func collect():
+		GameEvents.emit_exp_collected(VALUE, global_position)
+		queue_free()
+
+
+func disable_collision():
+	collision_shape_2d.disabled = true	
+	
 	
 func on_area_entered(other_area: Area2D):
-	GameEvents.emit_coin_collected(VALUE, global_position)
-	queue_free()
+	$AnimationPlayer.play("moving")
+	Callable(disable_collision).call_deferred()
+	
+	var tween = create_tween()
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, .8)\
+		.set_ease(Tween.EASE_IN)\
+		.set_trans(Tween.TRANS_BACK)
+	tween.tween_callback(collect)
