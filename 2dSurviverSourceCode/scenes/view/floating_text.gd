0324ecfb13a8784f846f44b85f196e5b76323209
IvanMovchanets <iv.movchanets@gmail.com>
2024-03-09 12:23:48 +0200

    fix float text

--- 2dSurviverSourceCode/scenes/view/floating_text.gd
+++ 2dSurviverSourceCode/scenes/view/floating_text.gd
@@ -16,15 +16,10 @@ func start(text: String):
 	
 	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 16), .3)\
 	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, "scale", Vector2.ZERO, .4)\
+	tween.tween_property(self, "scale", Vector2.ONE * 1.5, .3)\
 	.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 	tween.chain()
-	# // from lesson but I dont like this part
-	#tween.tween_property(self, "global_position", global_position + (Vector2.UP.rotated(randf_range(-2, 2)) * 48), .4)\
-	#.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
-	#tween.tween_property(self, "scale", Vector2.ZERO, .4)\
-	#.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
-	#tween.chain()
-	#
+	tween.tween_property(self, "scale",Vector2.ZERO, .4)
+	
 	tween.tween_callback(queue_free)
 
